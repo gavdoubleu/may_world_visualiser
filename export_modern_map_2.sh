b5298f153275f1621aa9b4656b592b37bc158654
gavdoubleu <gavin.a.woolman@durham.ac.uk>
2026-05-14 10:32:58 +0100

    Moved to the new map

---
+++ export_modern_map_2.sh
@@ -0,0 +1,2 @@
+python export_static.py --world-file ../june_zero/world_2021.h5 --map-background image --map-image data/terrain_noshade_log_UTM.png --map-bounds "56.13493,1.99441,49.74491,-7.05355" --map-attribution "Elevation map" --max-size-mb 300 --title "Modern Day" --output outputs/modern_map_2.html 2>&1 | tee log.txt
+#  --map-bounds "55.05603,1.99298,49.73103,-6.38048"
